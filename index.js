c51a3fcbc3a95119e960a28c5dd83f8be1d683c1
Lada <lada.goncharova@gmail.com>
2022-08-06 02:10:42 +0300

    Add Todo

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const list = document.getElementById('tasks');
 const btnAdd = document.getElementById('btnAdd');
 const btnDel = document.getElementById('btnDel')
 const task = document.getElementById('task');
+const date = document.getElementById('date');
+
+
+function zero_first_format(value)
+    {
+        if (value < 10)
+        {
+            value='0'+value;
+        }
+        return value;
+    }
+
+function date_time()
+    {
+      var current_datetime = new Date();
+      var day = zero_first_format(current_datetime.getDate());
+      var month = zero_first_format(current_datetime.getMonth()+1);
+      var year = current_datetime.getFullYear();
+
+      return day+"."+month+"."+year;
+    }
+    document.getElementById('dateBlock').innerHTML = date_time();
+
 
 btnAdd.addEventListener('click', addNewTask);
 btnDel.addEventListener('click', deleteCheckTasks);
@@ -10,19 +33,25 @@ btnDel.addEventListener('click', deleteCheckTasks);
 function addNewTask (event) {
   const item = document.createElement('li');
   item.innerText = task.value;
-  item.append(addChekbox())
+  item.append(dateNow());
+  item.append(addChekbox());
   list.append(item);
-  task.value = '';
+  task.value = ''
 }
 
 function addChekbox(){
 const checkElem = document.createElement('input');
 checkElem.type = 'checkbox';
 checkElem. classList.add('check');
-return checkElem;
+return checkElem
 }
 
 function deleteCheckTasks(event){
 const deleteTasks = document.querySelectorAll('input[type="checkbox"]:checked');
 deleteTasks.forEach(el => el.parentNode.remove());
+}
+function dateNow(){
+  let options = { weekday: 'long', month: 'short', day: 'numeric'};
+  let dateToday = new Date().toLocaleDateString('en-US', options);
+   return dateToday
 }
